316fc92dcac9d500b4ad144951a1dcc1b138cccf
prannou <prannou29@gmail.com>
2012-11-20 20:48:07 +0100

    Add comment and modify the position of the map

--- tinterface/src/main/java/org/orbisgis/tinterface/main/MainScene.java
+++ tinterface/src/main/java/org/orbisgis/tinterface/main/MainScene.java
@@ -14,141 +14,146 @@ import org.mt4j.util.math.Vector3D;
 /**
  * This class create the main scene from the application. The map, the layer
  * list and the temporal line are added as son from this scene
- *
+ * 
  * @author patrick
- *
+ * 
  */
 public class MainScene extends AbstractScene {
 
-        /**
-         * The map
-         */
-        private Map map;
+	/**
+	 * The map
+	 */
+	private Map map;
 
-        public Map getMap() {
-                return map;
-        }
+	/**
+	 * The layer list
+	 */
+	private LayerList layerList;
 
-        public void setMap(Map map) {
-                this.map = map;
-        }
-        /**
-         * The layer list
-         */
-        private LayerList layerList;
+	/**
+	 * The temporal line
+	 */
+	// private TemporalLine temporalLine;
 
-        /**
-         * The temporal line
-         */
-        // private TemporalLine temporalLine;
-        public LayerList getLayerList() {
-                return layerList;
-        }
+	/**
+	 * Constructor of MainScene
+	 * 
+	 * @param mtApplication
+	 *            the application
+	 * @param name
+	 *            the name of the scene
+	 * @throws Exception
+	 */
+	public MainScene(MTApplication mtApplication, String name) {
+		super(mtApplication, name);
 
-        public void setLayerList(LayerList layerList) {
-                this.layerList = layerList;
-        }
+		// Add a circle around every point that is touched
+		this.registerGlobalInputProcessor(new CursorTracer(mtApplication, this));
 
-        /**
-         * Constructor of MainScene
-         *
-         * @param mtApplication the application
-         * @param name the name of the scene
-         * @throws Exception 
-         */
-        public MainScene(MTApplication mtApplication, String name) {
-                super(mtApplication, name);
+		// Instantiate a new map with the default configuration (specify in a
+		// configuration file) and add it to the scene
+		try {
+			setMap(new Map(mtApplication, this));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-                // Add a circle around every point that is touched
-                this.registerGlobalInputProcessor(new CursorTracer(mtApplication, this));
+		// Instantiate the list of Layers
+		setLayerList(new LayerList(this, mtApplication));
 
-                // Instantiate a new map with the default configuration (specify in a
-                // configuration file) and add it to the scene
-                try {
-					setMap(new Map(mtApplication, this));
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		// Instantiate a new temporal line and add it to the scene
+		// temporalLine = new TemporalLine();
+		// this.getCanvas().addChild(temporalLine);
 
-                // Instantiate the list of Layers
-                setLayerList(new LayerList(this, mtApplication));
+		// Add the drag gesture on the map (the class MapDrag will be used)
+		map.registerInputProcessor(new DragProcessor(mtApplication));
+		map.addGestureListener(DragProcessor.class, new MapDrag());
 
-                // Instantiate a new temporal line and add it to the scene
-                // temporalLine = new TemporalLine();
-                // this.getCanvas().addChild(temporalLine);
+		// Add the scale gesture on the map (the class MapScale will be used)
+		map.registerInputProcessor(new ScaleProcessor(mtApplication));
+		map.addGestureListener(ScaleProcessor.class, new MapScale());
 
-                // Add the drag gesture on the map (the class MapDrag will be used)
-                map.registerInputProcessor(new DragProcessor(mtApplication));
-                map.addGestureListener(DragProcessor.class, new MapDrag());
+		// Add the tap and hold gesture on the map (with 3s hold)(the class
+		// MapTapAndHold will be used)
+		TapAndHoldProcessor tap = new TapAndHoldProcessor(mtApplication);
+		tap.setHoldTime(3000);
+		map.registerInputProcessor(tap);
+		map.addGestureListener(TapAndHoldProcessor.class, new MapTapAndHold());
 
-                // Add the scale gesture on the map (the class MapScale will be used)
-                map.registerInputProcessor(new ScaleProcessor(mtApplication));
-                map.addGestureListener(ScaleProcessor.class, new MapScale());
+	}
 
-                // Add the tap and hold gesture on the map (with 3s hold)(the class
-                // MapTapAndHold will be used)
-                TapAndHoldProcessor tap = new TapAndHoldProcessor(mtApplication);
-                tap.setHoldTime(3000);
-                map.registerInputProcessor(tap);
-                map.addGestureListener(TapAndHoldProcessor.class, new MapTapAndHold());
+	public Map getMap() {
+		return map;
+	}
 
-        }
+	public void setMap(Map map) {
+		this.map = map;
+	}
+	
+	public LayerList getLayerList() {
+		return layerList;
+	}
 
-        /**
-         * Nested class used when a drag gesture on the map is detected
-         *
-         * @author patrick
-         *
-         */
-        private class MapDrag implements IGestureEventListener {
+	public void setLayerList(LayerList layerList) {
+		this.layerList = layerList;
+	}
 
-                /**
-                 * Method called when a drag gesture is detected
-                 */
-                public boolean processGestureEvent(MTGestureEvent gesture) {
-                        // Get the translation vector
-                        Vector3D tVect = ((DragEvent) gesture).getTranslationVect();
-                        // Move the map
-                        map.move(tVect.x, tVect.y);
+	/**
+	 * Nested class used when a drag gesture on the map is detected
+	 * 
+	 * @author patrick
+	 * 
+	 */
+	private class MapDrag implements IGestureEventListener {
 
-                        return false;
-                }
-        }
+		/**
+		 * Method called when a drag gesture is detected
+		 */
+		public boolean processGestureEvent(MTGestureEvent gesture) {
+			// Get the translation vector
+			Vector3D tVect = ((DragEvent) gesture).getTranslationVect();
+			// Move the map
+			map.move(tVect.x, tVect.y);
 
-        /**
-         * Nested class used when a scale gesture on the map is detected
-         *
-         * @author patrick
-         *
-         */
-        private class MapScale implements IGestureEventListener {
+			return false;
+		}
+	}
 
-                /**
-                 * Method called when a scale gesture is detected
-                 */
-                public boolean processGestureEvent(MTGestureEvent gesture) {
-                        // Scale the map
-                        return false;
-                }
-        }
+	/**
+	 * Nested class used when a scale gesture on the map is detected
+	 * 
+	 * @author patrick
+	 * 
+	 */
+	private class MapScale implements IGestureEventListener {
 
-        /**
-         * Nested class used when a tap and hold gesture on the map is detected
-         *
-         * @author patrick
-         *
-         */
-        private class MapTapAndHold implements IGestureEventListener {
+		/**
+		 * Method called when a scale gesture is detected
+		 */
+		public boolean processGestureEvent(MTGestureEvent gesture) {
+			// Scale the map
+			return false;
+		}
+	}
 
-                /**
-                 * Method called when a scale gesture is detected
-                 */
-                public boolean processGestureEvent(MTGestureEvent gesture) {
-                        if (gesture.getId() == MTGestureEvent.GESTURE_ENDED) {
-                                //map.getInfos(((TapAndHoldEvent) gesture).getLocationOnScreen());
-                        }
-                        return false;
-                }
-        }
+	/**
+	 * Nested class used when a tap and hold gesture on the map is detected
+	 * 
+	 * @author patrick
+	 * 
+	 */
+	private class MapTapAndHold implements IGestureEventListener {
+
+		/**
+		 * Method called when a scale gesture is detected
+		 */
+		public boolean processGestureEvent(MTGestureEvent gesture) {
+			if (gesture.getId() == MTGestureEvent.GESTURE_ENDED) {
+				// map.getInfos(((TapAndHoldEvent)
+				// gesture).getLocationOnScreen());
+			}
+			return false;
+		}
+	}
 }
--- tinterface/src/main/java/org/orbisgis/tinterface/main/Map.java
+++ tinterface/src/main/java/org/orbisgis/tinterface/main/Map.java
@@ -6,8 +6,6 @@ import java.io.File;
 import java.io.InputStream;
 
 import org.mt4j.MTApplication;
-import org.mt4j.components.MTComponent;
-import org.mt4j.components.visibleComponents.AbstractVisibleComponent;
 import org.mt4j.components.visibleComponents.shapes.MTRectangle;
 import org.mt4j.util.MTColor;
 import org.mt4j.util.math.Vector3D;
@@ -20,48 +18,56 @@ import org.orbisgis.core.workspace.CoreWorkspace;
 import processing.core.PGraphics;
 import processing.core.PImage;
 
-import com.modestmaps.providers.AbstractMapProvider;
-
+/**
+ * Constructor of the class Map
+ * 
+ * @author patrick
+ * 
+ */
 public class Map extends MTRectangle {
-	public Map(MTApplication mtApplication, MainScene mainScene) throws Exception{
-        super(mtApplication, 1300, 710);
-        
-//        MainContext.initConsoleLogger(true);
-//        CoreWorkspace workspace = new CoreWorkspace();
-//        File workspaceFolder = new File(System.getProperty("user.home"), "OrbisGIS_MT" + File.separator);
-//        workspace.setWorkspaceFolder(workspaceFolder.getAbsolutePath());
-//        MainContext mainContext = new MainContext(true, workspace, true);
-//        final MainFrame frame = new MainFrame();
-//        final MapContext mapContext = getSampleMapContext();
-//        BufferedImage im = frame.mapTransform.getImage();
-//        PImage image = new PImage(im);
+	public Map(MTApplication mtApplication, MainScene mainScene)
+			throws Exception {
+		super(mtApplication, 1300, 710);
+		this.unregisterAllInputProcessors();
 
-        
+		MainContext.initConsoleLogger(true);
+		CoreWorkspace workspace = new CoreWorkspace();
+		File workspaceFolder = new File(System.getProperty("user.home"),
+				"OrbisGIS_MT" + File.separator);
+		workspace.setWorkspaceFolder(workspaceFolder.getAbsolutePath());
+		MainContext mainContext = new MainContext(true, workspace, true);
+		final MainFrame frame = new MainFrame();
+		final MapContext mapContext = getSampleMapContext();
+		frame.init(mapContext);
+
+		BufferedImage im = frame.mapTransform.getImage();
+		PImage image = new PImage(im);
+
 		this.setFillColor(new MTColor(0, 255, 0));
 		this.setPickable(false);
-		//this.setTexture(image);
-		//this.setPositionGlobal(new Vector3D(mtApplication.width, mtApplication.height));
-        //this.translateGlobal(new Vector3D(-mtApplication.width / 2f - 80, -10)); // Initializations position of the menu
-        mainScene.getCanvas().addChild(this);
+		this.setTexture(image);
+		mainScene.getCanvas().addChild(this);
 	}
 
+	/**
+	 * Method used to move the map from the parameter in input
+	 * 
+	 * @param x
+	 *            the number of pixel the map need to be moved (in x)
+	 * @param y
+	 *            the number of pixel the map need to be moved (in y)
+	 */
 	public void move(float x, float y) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	public void setMapProvider(AbstractMapProvider mapProvider) {
-		// TODO Auto-generated method stub
-		
+	private static MapContext getSampleMapContext()
+			throws IllegalStateException, LayerException {
+		MapContext mapContext = new OwsMapContext();
+		InputStream fileContent = Map.class.getResourceAsStream("Iris.ows");
+		mapContext.read(fileContent);
+		mapContext.open(null);
+		return mapContext;
 	}
-
-
-
-	private static MapContext getSampleMapContext() throws IllegalStateException, LayerException {
-        MapContext mapContext = new OwsMapContext();
-        InputStream fileContent = Map.class.getResourceAsStream("Iris.ows");
-        mapContext.read(fileContent);
-        mapContext.open(null);
-        return mapContext;
-}
 }
